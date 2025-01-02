84ab66030534ac32bf5d87f75f9d1ceb1d839759
LAKSHYA CHOUDHARY <151423785+LAKSHYACH543@users.noreply.github.com>
2025-01-02 14:33:36 +0530

    datatype.java

---
+++ datatype.java
@@ -0,0 +1,20 @@
+public class datatype {
+    public static void main(String[] args) {
+        int rollno=52;
+        //string is not primitive data type
+        String name="Lakshya choudhary";
+        char grade='a';
+        float marks=97.67f;
+        double cgpa=8.6574d;
+        long universityrollno=2300330100152L;
+        boolean check=true;
+        System.out.println(rollno);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(marks);
+        System.out.println(cgpa);
+        System.out.println(universityrollno);
+        System.out.println(check);
+    }
+    
+}
