ebd09d09942f56318f727c07328dc05d14cef453
LAKSHYA CHOUDHARY <151423785+LAKSHYACH543@users.noreply.github.com>
2025-01-02 14:32:36 +0530

    helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+    public static void main(String[]args){
+        System.out.println(args[0]);
+        System.out.print("my name is lakshya choudhary    ");
+        System.out.println("hello world!   ");
+        
+    }
+}
