430d98ed924404433d6cd04002b5b80a189b0339
LAKSHYA CHOUDHARY <151423785+LAKSHYACH543@users.noreply.github.com>
2025-02-22 11:11:48 +0530

    odd_or_even.java

---
+++ odd_or_even.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class oddoreven {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int a =in.nextInt();
+        if(a%2==0){
+            System.out.println("it is a even number");
+
+        }
+        else{
+            System.out.println("it is odd number");
+        }
+    }
+    
+}
