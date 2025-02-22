fb2c9c66a07f63bba85ce407062a0fa712559655
LAKSHYA CHOUDHARY <151423785+LAKSHYACH543@users.noreply.github.com>
2025-02-22 11:16:36 +0530

    greetingswithname.java

---
+++ greetingswithname.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class greetings_with_name {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter your full name");
+        String s=in.nextLine();
+        System.out.println("namaste dear "+s+" we grandly welcome you here");
+    }
+    
+}
